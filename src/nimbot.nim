c2b258b9f113db2923542296e9642d1c3a0b23e7
Peter Munch-Ellingsen <peterme@peterme.net>
2020-06-02 16:32:40 +0200

    Rename bot back

--- src/nimbot.nim
+++ src/nimbot.nim
@@ -19,7 +19,7 @@ const
   ircServer = "irc.freenode.net"
   joinChans = @["#nim", "#nimbuild"]
   announceChans = @["#nimbuild"]
-  botNickname = "NimBotNg"
+  botNickname = "NimBot"
 
 proc getCommandArgs(state: State) =
   for kind, key, value in getOpt():
